bdf765fe35686542ad77c6ecf7e13a4db8cb1257
Kristsina Sluchko <Kristinochka.09@mail.ru>
2021-12-11 01:50:18 +0300

    zadacha 3

---
+++ home_work_1/src/Number3.java
@@ -0,0 +1,12 @@
+public class Number3 {
+    public static void main(String[] args) {
+        System.out.println(Math.round(2.7)); // 3
+        System.out.println(Math.round(6.2)); //6
+        System.out.println(Math.floor(8.7)); //8
+        System.out.println(Math.ceil(5.2)); //6
+        System.out.println(Math.random());// выводит рандомное число >=0 и <1
+        System.out.println(Math.abs(-3));// считает модуль числа
+        System.out.println(Math.max(33,44));// находит максимальное число
+        System.out.println(Math.min(3,88));// находит минимальное число
+    }
+}
---
+++ home_work_1/src/Number5.java
@@ -0,0 +1,6 @@
+public class Number5 {
+     public static void main(String[] args) {
+
+
+    }
+}
